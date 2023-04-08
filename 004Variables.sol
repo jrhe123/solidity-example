694f02acddfd23ebe9c6dad52ca5fa3289bead5e
jrhe1213 <jiarong1213@gmail.com>
2023-04-07 21:50:33 -0400

    variables: local / block chain / global

--- 004Variables.sol
+++ 004Variables.sol
@@ -5,17 +5,32 @@ contract Variables {
     string public text = "Hello";
     uint public num = 123;
 
+    // get min/max by using type
     int8 public max = type(int8).max;
     int8 public min = type(int8).min;
 
-    function doSomething() public view returns (uint) {
-        // Local variables are not saved to the blockchain.
+    // 1. local variable: stored in function ram
+    // 2. block chain variable: stored in chain (needs gas)
+    // 3. global variable: global variable (ETH)
+
+    // e.g., stored in block chain
+    string public myName = "Hello world!!";
+
+    function doSomething() public view returns (uint, address) {
+        // e.g., local variables are not saved to the blockchain.
         uint i = 456;
 
-        // Here are some global variables
+        // block.timestamp
+        // block.number
+        // block.gaslimit
+        // block.difficulty
+        // block.coinbase
+        // block.blockhash(blockNumber);
+
+        // e.g., global variables
         uint timestamp = block.timestamp; // Current block timestamp
         address sender = msg.sender; // address of the caller
 
-        return 123;
+        return (timestamp, sender);
     }
 }
